da6fe91da90d3b89de9f06eebbf655fbb3f4dc27
chava <chava@chavas-Mac.local>
2015-06-11 17:17:47 -0700

    Initial Commit

---
+++ CM-init2/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CM-init2
+//
+//  Created by chava on 6/11/15.
+//  Copyright (c) 2015 fuentess. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
